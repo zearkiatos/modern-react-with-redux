37af2a172b30dd7f1c1ec6a25408570f12e6cc10
Pedro Capriles <caprilespe@outlook.com>
2020-07-12 17:43:08 -0400

    Reat's props system, passing multiple props, passing props, component reuse and implementing an approval card

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import faker from 'faker';
+import React from "react";
 
-const Comment = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        {faker.name.firstName()}
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                <div className="text">Nice Blog Post</div>
-                </div>
-            </div>
+const Comment = (props) => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={props.avatar} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {props.author}
+          </a>
+          <div className="metadata">
+            <span className="date">{props.timeAgo}</span>
+          </div>
+          <div className="text">{props.content}</div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Comment;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
 // Import the React and ReactDOM Libraies
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import Comment from "./components/Comment";
+import ApprovalCard from "./components/ApprovalCard";
 
 // Create a react component
 const App = () => {
-  const buttonText = {text: 'Click Me'};
-  const labelText = 'Enter Name:';
+  const buttonText = { text: "Click Me" };
+  const labelText = "Enter Name:";
   return (
     <Fragment>
-      <label className="label" htmlFor="name">
-        {labelText}
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {buttonText.text}
-      </button>
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
-      <Comment />
+      <div>
+        <label className="label" htmlFor="name">
+          {labelText}
+        </label>
+        <input id="name" type="text" />
+        <button style={{ backgroundColor: "blue", color: "white" }}>
+          {buttonText.text}
+        </button>
+      </div>
+      <ApprovalCard />
+      <Comment
+        author={faker.name.firstName()}
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+        content={faker.lorem.text()}
+      />
+      <Comment
+        author={faker.name.firstName()}
+        timeAgo="Today at 2:00AM"
+        avatar={faker.image.avatar()}
+        content={faker.lorem.text()}
+      />
+      <Comment
+        author={faker.name.firstName()}
+        timeAgo="Yesterday at 5:00PM"
+        avatar={faker.image.avatar()}
+        content={faker.lorem.text()}
+      />
     </Fragment>
   );
 };
