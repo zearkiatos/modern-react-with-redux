896f44fbab1134e5eeb04edbbd62270898ad30e6
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2020-08-09 15:03:37 -0400

    Handling errors gracefully

--- src/components/Season/index.js
+++ src/components/Season/index.js
@@ -5,21 +5,37 @@ class Season extends React.Component {
         super(props);
         this.state = {
             latitude: 0,
-            longitude: 0
+            longitude: 0,
+            errorMessage: '',
+            error: false
         };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({
-                    latitude: position.coords.latitude
+                    latitude: position.coords.latitude,
+                    error: false,
+                    errorMessage: ''
                 })
             },
-            (err) => console.error(err)
+            (err) => {
+                console.error(err);
+                this.setState({
+                    errorMessage: err.message,
+                    error: true
+                })
+            }
         );
     }
 
     render(){
-        return <div>Latitude: {this.state.latitude}</div>;
+        return (
+            <div>
+                Latitude: {this.state.latitude}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+        )
     }
 }
 
