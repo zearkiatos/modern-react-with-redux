93caf57b2a773f36c043442ea8e8aefa991cfccb
Pedro Capriles <caprilespe@outlook.com>
2020-08-17 10:54:37 -0400

    Refactoring data loading to lifecycle methods

--- src/components/Season/index.js
+++ src/components/Season/index.js
@@ -1,54 +1,50 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 class Season extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            latitude: 0,
-            longitude: 0,
-            errorMessage: '',
-            error: false
-        };
-        this.initialization()
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      errorMessage: "",
+      error: false,
+    };
+  }
 
-    initialization() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({
-                    latitude: position.coords.latitude,
-                    error: false,
-                    errorMessage: ''
-                })
-            },
-            (err) => {
-                console.error(err);
-                this.setState({
-                    errorMessage: err.message,
-                    error: true
-                })
-            }
-        );
-    }
+  initialization() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) =>
+        this.setState({
+          latitude: position.coords.latitude,
+          error: false,
+          errorMessage: "",
+        }),
+      (err) =>
+        this.setState({
+          errorMessage: err.message,
+          error: true,
+        })
+    );
+  }
 
-    render() {
-        if (this.state.error && !this.state.latitude)
-            return (
-            <Fragment>
-                <span> Error: </span>
-                {this.state.errorMessage}
-            </Fragment>
-            )
+  componentDidMount() {
+    this.initialization();
+  }
 
-        if (!this.state.error && this.state.latitude)
-            return (
-                <div>
-                    Latitude: {this.state.latitude}
-                </div>
-            )
-        
-            return <div>Loading... </div>
-    }
+  render() {
+    if (this.state.error && !this.state.latitude)
+      return (
+        <Fragment>
+          <span> Error: </span>
+          {this.state.errorMessage}
+        </Fragment>
+      );
+
+    if (!this.state.error && this.state.latitude)
+      return <div>Latitude: {this.state.latitude}</div>;
+
+    return <div>Loading... </div>;
+  }
 }
 
 export default Season;
