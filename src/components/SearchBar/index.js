57ac2566d5bc0862836a593cbfb757aa90402ed4
Pedro Capriles <caprilespe@outlook.com>
2020-09-13 15:44:49 -0300

    Uncontrolled vs Controlled Elements, more on controlled elements

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange = (event) => {
-    console.log(event.target.value);
+  state = {
+    term: "",
   };
-
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })}
+            />
           </div>
         </form>
       </div>
