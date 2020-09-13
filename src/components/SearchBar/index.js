c1ab04cc2f76c1cfe779d42ed5ecccfab5d49020
Pedro Capriles <caprilespe@outlook.com>
2020-09-13 15:49:36 -0300

    Handling form submittal

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -4,16 +4,21 @@ class SearchBar extends React.Component {
   state = {
     term: "",
   };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  };
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               value={this.state.term}
-              onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })}
+              onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
